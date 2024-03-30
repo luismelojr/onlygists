895f0e7bb0e8694b0b5403d7fb192d0c98cd36aa
Luis Henrique <junimhs1996@gmail.com>
2024-03-30 13:08:52 -0300

    feat: setup eslint and prettier

--- app.vue
+++ app.vue
@@ -1,9 +1,8 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <div>
     <h1 class="font-[Inter] text-primary-500">Only Gists</h1>
-    <Button label="Submit" loading/>
+    <Button label="Submit" loading />
   </div>
 </template>
